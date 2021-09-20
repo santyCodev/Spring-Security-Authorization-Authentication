7d409cae049f9ce1979edb0788a8c81d244c3149
Santy <santi.codev@gmail.com>
2021-09-20 12:52:13 +0200

    DEV: Add logs in userServiceImpl

--- src/main/java/com/santicodev/SpringSecurityauthorizationautentication/service/UserServiceImpl.java
+++ src/main/java/com/santicodev/SpringSecurityauthorizationautentication/service/UserServiceImpl.java
@@ -19,16 +19,19 @@ public class UserServiceImpl implements UserService{
 
     @Override
     public User saveUser(User user) {
+        log.info("Saving new user {} to the database", user.getName());
         return userRepository.save(user);
     }
 
     @Override
     public Role saveRole(Role role) {
+        log.info("Saving new role {} to the database", role.getName());
         return roleRepository.save(role);
     }
 
     @Override
     public void addRoleToUser(String username, String roleName) {
+        log.info("Adding role {} to user {}", roleName, username);
         User user = userRepository.findByUsername(username);
         Role role = roleRepository.findByName(roleName);
         user.getRoles().add(role);
@@ -36,11 +39,13 @@ public class UserServiceImpl implements UserService{
 
     @Override
     public User getUser(String username) {
+        log.info("Fetching user {}", username);
         return userRepository.findByUsername((username));
     }
 
     @Override
     public List<User> getUsers() {
+        log.info("Fetching all user");
         return userRepository.findAll();
     }
 }
