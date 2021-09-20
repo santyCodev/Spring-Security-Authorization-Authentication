1d41a884cb63e731d30c75767d0cee2c734a1bec
Santy <santi.codev@gmail.com>
2021-09-20 12:05:32 +0200

    DEV: Primer commit

---
+++ src/main/java/com/santicodev/SpringSecurityauthorizationautentication/SpringSecurityAuthorizationAutenticationApplication.java
@@ -0,0 +1,13 @@
+package com.santicodev.SpringSecurityauthorizationautentication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityAuthorizationAutenticationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityAuthorizationAutenticationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/santicodev/SpringSecurityauthorizationautentication/SpringSecurityAuthorizationAutenticationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.santicodev.SpringSecurityauthorizationautentication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityAuthorizationAutenticationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
