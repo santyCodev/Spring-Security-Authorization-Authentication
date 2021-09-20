5a56b2692d7aa7c93a0171acebf316b383753bcf
Santy <santi.codev@gmail.com>
2021-09-20 15:34:10 +0200

    DEV: class securityConfig

--- src/main/java/com/santicodev/SpringSecurityauthorizationautentication/security/SecurityConfig.java
+++ src/main/java/com/santicodev/SpringSecurityauthorizationautentication/security/SecurityConfig.java
@@ -1,4 +1,28 @@
 package com.santicodev.SpringSecurityauthorizationautentication.security;
 
-public class SecurityConfig {
+import lombok.RequiredArgsConstructor;
+import org.aspectj.weaver.BCException;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+@Configuration @EnableWebSecurity @RequiredArgsConstructor
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private final UserDetailsService userDetailsService;
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        super.configure(auth);
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        super.configure(http);
+    }
 }
